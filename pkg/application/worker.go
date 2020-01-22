46a630590e28ff3d83491b28b5195f13eebe0fcd
Fernando Ocampo <fernando.ocampo@avantica.net>
2020-01-21 22:00:46 -0500

    add fanin

--- pkg/application/worker.go
+++ pkg/application/worker.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/fernandoocampo/processor/pkg/domain"
 )
@@ -41,6 +42,36 @@ func Process(ctx context.Context, filepath string) ([]*domain.Employee, error) {
 		result = append(result, v.employee)
 	}
 	return result, nil
+}
+
+func fanIn(done <-chan interface{}, channels ...<-chan interface{}) <-chan interface{} {
+	var wg sync.WaitGroup
+	multiplexedStream := make(chan interface{})
+
+	multiplex := func(c <-chan interface{}) {
+		defer wg.Done()
+		for i := range c {
+			select {
+			case <-done:
+				return
+			case multiplexedStream <- i:
+			}
+		}
+	}
+
+	// Select from all the channels
+	wg.Add(len(channels))
+	for _, c := range channels {
+		go multiplex(c)
+	}
+
+	// Wait for all the reads to complete
+	go func() {
+		wg.Wait()
+		close(multiplexedStream)
+	}()
+
+	return multiplexedStream
 }
 
 // getRecords create stream to get the records from file
